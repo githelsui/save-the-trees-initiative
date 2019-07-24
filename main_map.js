646ca5bd4bee6c104b2b0eeff86e05756cea89d7
Githel Suico <githelsuico@gmail.com>
2019-07-23 18:18:09 -0700

    fixed map interface

--- main_map.js
+++ main_map.js
@@ -1,4 +1,15 @@
-var map = L.map('map').setView([-41.2858, 174.78682], 14);
+const treeicn = L.icon({
+    iconUrl: 'https://img.icons8.com/color/52/000000/deciduous-tree.png',
+    // shadowUrl: 'https://img.icons8.com/bubbles/26/000000/deciduous-tree.png',
+
+    iconSize:     [56, 56], // size of the icon
+    // shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [24, 56], // point of the icon which will correspond to marker's location
+    // shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var map = L.map('canvas').setView([34.27083595165, -117.740478515625], 14);
 mapLink =
     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
@@ -6,3 +17,34 @@ L.tileLayer(
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
+
+map.on('click', function(e){
+  L.marker(e.latlng, {icon: treeicn}).addTo(map);
+  console.log(e.latlng);
+});
+
+// loadData();
+
+// function loadData(all){
+//   for each(marker in all){
+//     L.marker(marker).addTo(map)
+//   }
+// }
+
+function saveData(){
+  var allMarkers = getAllMarkers();
+  saveMarkers(allMarkers);
+  //calls method on the web
+}
+
+function getAllMarkers(){
+  var allMarkers = [];
+  map.eachLayer(function (layer){
+    if(layer.options.icon != undefined){
+      allMarkers.add(layer.latlng);
+      allMarkers.push({
+        markerId: layer.options.markerId
+      });
+    }
+  })
+}
