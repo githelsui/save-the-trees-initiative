d527818d1d98319d621db277dade19dd4c2e7a6e
Githel Suico <githelsuico@gmail.com>
2019-07-23 17:10:56 -0700

    add gitignore file

---
+++ main_map.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([-41.2858, 174.78682], 14);
+mapLink =
+    '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; ' + mapLink + ' Contributors',
+    maxZoom: 18,
+    }).addTo(map);
