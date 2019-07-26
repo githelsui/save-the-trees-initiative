27b62749bda3b16176d20366512f223aaf40ff08
Githel Suico <githelsuico@gmail.com>
2019-07-26 10:47:29 -0700

    yee

--- models.py
+++ models.py
@@ -3,5 +3,6 @@ from google.appengine.ext import ndb
 class Tree(ndb.Model):
     lat = ndb.FloatProperty(required=True)
     long = ndb.FloatProperty(required=True)
+    number = ndb.FloatProperty(required=True)
     user_id = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
